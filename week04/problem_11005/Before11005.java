eb35cb8ad5471519f914ea8c21d5e823c39e8fb3
conycomy <imgyeong4936@gmail.com>
2025-09-12 01:31:34 +0900

    feat: 백준 11005 풀이 및 오답정리

---
+++ week04/problem_11005/Before11005.java
@@ -0,0 +1,28 @@
+package week04.problem_11005;
+
+import java.util.Scanner;
+
+public class Before11005 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt(); // 10진법 수 N
+		int B = in.nextInt();
+
+		int count = 0;
+		char[] ch = new char[32];
+		while ( N > 0 ){
+			int MiddleResult = N % B;
+			if (MiddleResult < 10) {
+				ch[count++] = (char)('0' + (MiddleResult));
+			} else {
+				ch[count++] = (char)('A' + (MiddleResult - 10));
+			}
+			N /= B;
+		}
+		for(int i = count -1; i>=0; i--){
+			System.out.print(ch[i]);
+		}
+	}
+
+}
